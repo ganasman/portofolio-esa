9584dd34cb78849aa34f9bd973bb09e143bb72fa
ganasman <ganasman3@gmail.com>
2026-01-09 09:28:25 +0700

    add @vercel/speed-insights dependency and import in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 type Certificate = {
   title: string;
